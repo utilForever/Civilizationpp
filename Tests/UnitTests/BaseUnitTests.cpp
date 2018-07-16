fb5b899d0574cc86be5dc1ac1104338767e87bfe
Las-Wonho <haho013039@gmail.com>
2018-07-16 13:19:21 +0900

    Add TestCases

--- Tests/UnitTests/BaseUnitTests.cpp
+++ Tests/UnitTests/BaseUnitTests.cpp
@@ -9,4 +9,10 @@ class BaseUnitTest : public ::testing::Test {
 TEST_F(BaseUnitTest, BaseUnitShuldCanMove) {
     auto position = baseunit.Move(+1, -3);
     ASSERT_DOUBLE_EQ(position.first == +1, position.second == -3);
+    position = baseunit.Move(-1, +3);
+    ASSERT_DOUBLE_EQ(position.first == -1, position.second == +3);
+    position = baseunit.Move(-1, +30);
+    ASSERT_DOUBLE_EQ(position.first == -1, position.second == +30);
+    position = baseunit.Move(-13, +30);
+    ASSERT_DOUBLE_EQ(position.first == -13, position.second == +30);
 }
