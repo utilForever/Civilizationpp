4c8ee5c1200ddbb12678fa8197ebacec1bf0e3c2
Las-Wonho <haho013039@gmail.com>
2018-07-16 20:11:40 +0900

    Fix Obtainable Signiture

--- Includes/Material.h
+++ Includes/Material.h
@@ -1,8 +1,9 @@
+#include <HexTile.h>
 namespace Civilizationpp {
 	class Obtainable {
 	public:
-        virtual int GetFood() = 0;
-        virtual int GetProduction() = 0;
-        virtual int GetGold() = 0;
+        virtual int GetFood(HexTile tile) = 0;
+        virtual int GetProduction(HexTile tile) = 0;
+        virtual int GetGold(HexTile tile) = 0;
     };
 }
