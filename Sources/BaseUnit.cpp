a2fbd3da87df15eb9a487412db73ef81305d0b1a
Las-Wonho <haho013039@gmail.com>
2018-07-16 14:33:56 +0900

    Add Implementation BaseUnit constructor

--- Sources/BaseUnit.cpp
+++ Sources/BaseUnit.cpp
@@ -1,5 +1,9 @@
 #include <BaseUnit.h>
 namespace Civilizationpp {
+BaseUnit::BaseUnit(int x, int y){ 
+	m_positionX = x;
+    m_positionY = y;
+} 
 std::pair<int,int> BaseUnit::Move(int x, int y) {
     m_positionX = x;
     m_positionY = y;
