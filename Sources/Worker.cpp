6989cb2f3efd047d4e64865342ad5e8a649c44a4
Las-Wonho <haho013039@gmail.com>
2018-07-17 17:18:34 +0900

    Add Worker Implementation

--- Sources/Worker.cpp
+++ Sources/Worker.cpp
@@ -1,5 +1,11 @@
 #include <Worker.h>
 using namespace Civilizationpp;
-void Worker::Build(Obtainable* tile) {
+void Worker::Build(Obtainable* tile) { 
+	tile->ChangeFood([](HexTile t)->int { return 1; });
+}
+void Worker::Build(Obtainable* tile, eventFunction f) {
+    tile->ChangeFood(f);
+}
+Worker::Worker(int x, int y) : BaseUnit(x,y) {
 
 }
