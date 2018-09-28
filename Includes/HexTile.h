6e1441f9f0e6b1323c10df3bcc34cf42a0a3b6a8
Wonho ha <haho013039@gmail.com>
2018-09-28 16:16:30 +0900

    #9 Apply Terrain to Tile

--- Includes/HexTile.h
+++ Includes/HexTile.h
@@ -1,10 +1,21 @@
 #ifndef CIVILIZATIONPP_HEX_TILE_H
 #define CIVILIZATIONPP_HEX_TILE_H
 
-namespace Civilizationpp
-{
-    class HexTile {};
-}
 #include <Terrain.h>
+
+namespace Civilizationpp {
+	class HexTile {
+
+	private:
+
+		BasicTerrain m_Terrain;
+
+	public:
+
+		BasicTerrain GetTerrain() { return m_Terrain; }
+		void ChangeTerrain(BasicTerrain Terrain) { m_Terrain = Terrain; }
+
+	};
+}  // namespace Civilizationpp
 
 #endif  // CIVILIZATIONPP_HEX_TILE_H
