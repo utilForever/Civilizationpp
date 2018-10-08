04fc7b867c32645e60c549a20054f666f16d33ad
Wonho ha <haho013039@gmail.com>
2018-10-08 13:43:51 +0900

    Add TerrainName

--- Includes/Terrain.h
+++ Includes/Terrain.h
@@ -3,6 +3,25 @@
 
 namespace Civilizationpp {
 enum class BasicTerrain { Open, Rough, Lake, Coast, Ocean, Fresh_Water };
+enum class TerrainName {
+    Coast,
+    Desert,
+    GrassLand,
+    Hill,
+    Mountain,
+    Ocean,
+    Plains,
+    Snow,
+    Tundra,
+    Forest,
+    Jungle,
+    Marsh,
+    Atoll,
+    FloodPlains,
+    Ice,
+    LAkes,
+    Oasis
+};
 }  // namespace Civilizationpp
 
 #endif  // CIVILAZATIONPP_TERRAIN
