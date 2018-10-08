c8f7d5182bad6d1c33ba8618f63f1a3d83d2778f
Wonho ha <haho013039@gmail.com>
2018-10-08 13:43:03 +0900

    Add Resource and Deprecate Obtainable interface

---
+++ Includes/Resource.h
@@ -0,0 +1,11 @@
+#ifndef CIVILIZATIONPP_RESOURCE
+#define CIVILIZATIONPP_RESOURCE
+
+namespace Civilizationpp {
+struct Resource {
+    int Food;
+    int Production;
+    int Gold;
+};
+}  // namespace Civilizationpp
+#endif  // Resource
