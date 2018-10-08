0799b906a296e90099e4a7a901fa4b71b37efb7c
Wonho ha <haho013039@gmail.com>
2018-10-08 16:11:54 +0900

    Add Resource.cpp
    Getter를 정의한 파일을 추가하였습니다.

---
+++ Sources/Resource.cpp
@@ -0,0 +1,5 @@
+#include <Resource.h>
+
+int Civilizationpp::Resource::GetFood() const { return m_Food; }
+int Civilizationpp::Resource::GetProduction() const { return m_Production; }
+int Civilizationpp::Resource::GetGold() const { return m_Gold; }
