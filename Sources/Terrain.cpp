2ac1fcc0f31d144920ec6e45236cfd0852cccdc5
Wonho ha <haho013039@gmail.com>
2018-10-10 08:19:21 +0900

    Add Resource change method

--- Sources/Terrain.cpp
+++ Sources/Terrain.cpp
@@ -7,6 +7,9 @@ int Civilizationpp::Terrain::GetMovement() const { return m_Movement; }
 Civilizationpp::Resource Civilizationpp::Terrain::GetResource() const {
     return m_Resource;
 }
+void Civilizationpp::Terrain::ChangeResource(Resource resource) {
+    m_Resource = resource;
+}
 Civilizationpp::BasicTerrain Civilizationpp::Terrain::GetCategory() const {
     return m_Category;
 }
