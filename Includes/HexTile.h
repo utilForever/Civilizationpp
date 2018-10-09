8d19eebb2f18b94da3e88140354ad6be15e9cdd2
Wonho ha <haho013039@gmail.com>
2018-10-10 08:45:58 +0900

    Change HexTile

--- Includes/HexTile.h
+++ Includes/HexTile.h
@@ -4,18 +4,17 @@
 #include <Terrain.h>
 
 namespace Civilizationpp {
-	class HexTile {
+class HexTile {
+ private:
+    Terrain m_Terrain;
 
-	private:
-
-		Terrain m_Terrain;
-
-	public:
-
-		Terrain GetTerrain() { return m_Terrain; }
-		void ChangeTerrain(Terrain terrain) { m_Terrain = terrain; }
-
-	};
+ public:
+    Terrain GetTerrain() { return m_Terrain; }
+    void ChangeTerrain(Terrain terrain) { m_Terrain = terrain; }
+    void ChangeResource(Resource resource) {
+        m_Terrain.ChangeResource(resource);
+    }
+};
 }  // namespace Civilizationpp
 
 #endif  // CIVILIZATIONPP_HEX_TILE_H
