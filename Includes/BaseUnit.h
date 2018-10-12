9587e4459524057945aa4025522dc5d8db9afc42
Wonho ha <haho013039@gmail.com>
2018-10-12 11:52:14 +0900

    Change Unit struct

--- Includes/BaseUnit.h
+++ Includes/BaseUnit.h
@@ -10,8 +10,6 @@ class BaseUnit {
     int m_positionX{0};
     int m_positionY{0};
 
- protected:
-    HexMap* m_map;
  public:
     BaseUnit(int x, int y);
     virtual std::pair<int, int> Move(int x, int y) noexcept;
