75d8570e00fe00447a83c1a8b007eed0ac5eef07
Chris Ohk <utilForever@gmail.com>
2018-10-12 21:10:52 +0900

    fix: Change type (size_t -> std::size_t)

--- Includes/GameSettings.h
+++ Includes/GameSettings.h
@@ -1,15 +1,17 @@
 #ifndef CIVILIZATIONPP_GAME_SETTINGS_H
 #define CIVILIZATIONPP_GAME_SETTINGS_H
 
+#include <cstddef>
+
 namespace Civilizationpp
 {
-    struct GameSettings
-    {
-        GameSettings(size_t rowCount, size_t colCount);
+struct GameSettings
+{
+    GameSettings(std::size_t rowCount, std::size_t colCount);
 
-        size_t rowCount;
-        size_t colCount;
-    };
-}
+    std::size_t rowCount;
+    std::size_t colCount;
+};
+}  // namespace Civilizationpp
 
-#endif //CIVILIZATIONPP_GAME_SETTINGS_H
+#endif  // CIVILIZATIONPP_GAME_SETTINGS_H
