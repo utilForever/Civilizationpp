c72701538f6727dfba8f96b4421007da3578ea4f
Chris Ohk <utilForever@gmail.com>
2018-10-13 14:03:18 +0900

    Separate docker run command

--- Scripts/travis_build_docker.sh
+++ Scripts/travis_build_docker.sh
@@ -5,7 +5,8 @@ set -e
 if [ $# -eq 0 ]
   then
     docker build -t utilforever/civilizationpp .
+    docker run utilforever/civilizationpp
 else
     docker build -f $1 -t utilforever/civilizationpp:$2 .
+    docker run utilforever/civilizationpp:$2
 fi
-docker run utilforever/civilizationpp
