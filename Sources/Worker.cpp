e1ac18965e9465bd1ff7610fde4769742903f74e
Wonho ha <haho013039@gmail.com>
2018-10-29 21:11:49 +0900

    Fix Error Worker::Build

--- Sources/Worker.cpp
+++ Sources/Worker.cpp
@@ -5,9 +5,8 @@ using namespace Civilizationpp;
 Worker::Worker(int x, int y) : BaseUnit(x, y) {}
 
 void Worker::Build(Resource* resource) {
-    auto [x, y] = GetPosition();
-    
-	HexMap::GetInstance()->GetTile(x, y)->ChangeResource(resource);
+    auto position = GetPosition();
+	HexMap::GetInstance()->GetTile(position.first, position.second)->ChangeResource(resource);
 }
 
 void Worker::Build(HexTile* tile, Resource* resource)
