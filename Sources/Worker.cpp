fb32c0f2a33793b432c08a84e5bab6cb250f10bc
Wonho ha <haho013039@gmail.com>
2018-10-29 20:06:14 +0900

    Change implementaion of Worker::Build

--- Sources/Worker.cpp
+++ Sources/Worker.cpp
@@ -4,10 +4,13 @@ using namespace Civilizationpp;
 
 Worker::Worker(int x, int y) : BaseUnit(x, y) {}
 
-void Worker::Build(Resource* tile) {
+void Worker::Build(Resource* resource) {
     auto [x, y] = GetPosition();
     
-	HexMap::GetInstance()->GetTile(x, y)->ChangeResource(tile);
+	HexMap::GetInstance()->GetTile(x, y)->ChangeResource(resource);
 }
 
-void Worker::Build(HexTile* tile, Resource* f) {}
+void Worker::Build(HexTile* tile, Resource* resource)
+{
+    tile->ChangeResource(resource);
+}
