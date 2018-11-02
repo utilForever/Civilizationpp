f48c709e79488220b2f3525e87589ef1f2961f3f
Wonho ha <haho013039@gmail.com>
2018-11-02 22:45:12 +0900

    Seperate UnitFactory Implementaion

--- Includes/UnitFactory.h
+++ Includes/UnitFactory.h
@@ -5,28 +5,26 @@
 #include <Setter.h>
 #include <Worker.h>
 
-namespace Civilizationpp {
-namespace Unit {
-enum class Codes { Setter = 0, Worker = 1 };
+namespace Civilizationpp
+{
+namespace Unit
+{
+enum class Codes
+{
+    Setter = 0,
+    Worker = 1
+};
 }
 
-class UnitFactory {
+class UnitFactory
+{
  private:
     int m_x;
     int m_y;
+
  public:
     UnitFactory(int x, int y);
-    auto CreatUnit(Unit::Codes unitCode) -> BaseUnit* {
-        BaseUnit* unit;
-        if (unitCode == Unit::Codes::Setter) {
-            unit = new Setter(m_x, m_y);
-            return unit;
-        } else if (unitCode == Unit::Codes::Worker) {
-            unit = new Worker(m_x, m_y);
-            return unit;
-        }
-        return nullptr;
-    };
+    BaseUnit* CreatUnit(Unit::Codes unitCode);
 };
 }  // namespace Civilizationpp
 #endif  // CIVILIZATIONPP_UNIT_FACTORY
--- Sources/UnitFactory.cpp
+++ Sources/UnitFactory.cpp
@@ -6,3 +6,19 @@ UnitFactory::UnitFactory(int x, int y)
     m_x = x;
     m_y = y;
 }
+
+BaseUnit* UnitFactory::CreatUnit(Unit::Codes unitCode)
+{
+    BaseUnit* unit;
+    if (unitCode == Unit::Codes::Setter)
+    {
+        unit = new Setter(m_x, m_y);
+        return unit;
+    }
+    else if (unitCode == Unit::Codes::Worker)
+    {
+        unit = new Worker(m_x, m_y);
+        return unit;
+    }
+    return nullptr;
+};
