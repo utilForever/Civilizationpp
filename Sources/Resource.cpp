85706b61ee33cd4a373cc3c490580096a5f82781
Wonho ha <haho013039@gmail.com>
2018-11-02 22:21:16 +0900

    Change Resource coding style

--- Sources/Resource.cpp
+++ Sources/Resource.cpp
@@ -1,10 +1,21 @@
 #include <Resource.h>
-Civilizationpp::Resource::Resource()
+using namespace Civilizationpp;
+
+Resource::Resource()
 {
     m_food = 0;
     m_gold = 0;
     m_production = 0;
-};
-int Civilizationpp::Resource::GetFood() const { return m_food; }
-int Civilizationpp::Resource::GetProduction() const { return m_production; }
-int Civilizationpp::Resource::GetGold() const { return m_gold; }
+}
+int Resource::GetFood() const
+{
+    return m_food;
+}
+int Resource::GetProduction() const
+{
+    return m_production;
+}
+int Resource::GetGold() const
+{
+    return m_gold;
+}
