a3a6367a3098d5af5b78c9884890f77d674872b1
Wonho ha <haho013039@gmail.com>
2018-11-02 22:35:45 +0900

    Seperate Tile Implementation

--- Includes/HexTile.h
+++ Includes/HexTile.h
@@ -5,29 +5,17 @@
 
 namespace Civilizationpp
 {
-class HexTile
-{
- private:
-    Terrain* m_pTerrain;
+	class HexTile
+	{
+	 private:
+		Terrain* m_pTerrain;
 
- public:
-    HexTile()
-    {
-        m_pTerrain = new Terrain();
-    }
-    Terrain* GetTerrain() const
-    {
-        return m_pTerrain;
-    }
-    void SetTerrain(Terrain* terrain)
-    {
-        m_pTerrain = terrain;
-    }
-    void ChangeResource(Resource* resource)
-    {
-        m_pTerrain->ChangeResource(resource);
-    }
-};
+	 public:
+		HexTile();
+		Terrain* GetTerrain() const;
+		void SetTerrain(Terrain* terrain);
+		void ChangeResource(Resource* resource);
+	};
 }  // namespace Civilizationpp
 
 #endif  // CIVILIZATIONPP_HEX_TILE_H
--- Sources/HexTile.cpp
+++ Sources/HexTile.cpp
@@ -1,1 +1,19 @@
 #include <HexTile.h>
+using namespace Civilizationpp;
+
+HexTile::HexTile()
+{
+    m_pTerrain = new Terrain();
+}
+Terrain* HexTile::GetTerrain() const
+{
+    return m_pTerrain;
+}
+void HexTile::SetTerrain(Terrain* terrain)
+{
+    m_pTerrain = terrain;
+}
+void HexTile::ChangeResource(Resource* resource)
+{
+    m_pTerrain->ChangeResource(resource);
+}
