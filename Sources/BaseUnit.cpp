b68ed1d838175727cd46f6b9e63b147d6da979b7
Wonho ha <haho013039@gmail.com>
2018-11-05 08:26:08 +0900

    refactor: Unit::Move

--- Sources/BaseUnit.cpp
+++ Sources/BaseUnit.cpp
@@ -10,8 +10,40 @@ BaseUnit::BaseUnit(int x, int y)
 
 void BaseUnit::Move(int x, int y) noexcept
 {
-    m_positionX = x;
-    m_positionY = y;
+    const int deffresceX = m_positionX - x;
+    const int deffresceY = m_positionY - y;
+
+    if (m_movement < abs(deffresceX))
+    {
+        if (deffresceX > 0)
+        {
+            m_positionX += m_movement;
+        }
+        else
+        {
+            m_positionX -= m_movement;
+        }
+    }
+    else
+    {
+        m_positionX = x;
+    }
+
+    if (m_movement < abs(deffresceY))
+    {
+        if (deffresceY > 0)
+        {
+            m_positionY += m_movement;
+        }
+        else
+        {
+            m_positionY -= m_movement;
+        }
+    }
+    else
+    {
+        m_positionY = y;
+    }
 }
 
 std::pair<int, int> BaseUnit::GetPosition()
