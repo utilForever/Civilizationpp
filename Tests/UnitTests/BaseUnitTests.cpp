945c25be422385021f5c1fa836fd3a76cdd66d89
Wonho ha <haho013039@gmail.com>
2018-11-05 08:34:50 +0900

    test: Fix Move and FactoryMethod

--- Tests/UnitTests/BaseUnitTests.cpp
+++ Tests/UnitTests/BaseUnitTests.cpp
@@ -8,15 +8,19 @@ TEST(BaseUnit, ShouldCanMove)
 {
     BaseUnit baseunit{ 1, 2 };
 
-    auto position = baseunit.Move(+1, -3);
+    baseunit.Move(+1, -3);
+    auto position = baseunit.GetPosition();
     ASSERT_TRUE(position.first == +1 && position.second == -3);
 
-    position = baseunit.Move(-1, +3);
+    baseunit.Move(-1, +3);
+    position = baseunit.GetPosition();
     ASSERT_TRUE(position.first == -1 && position.second == +3);
 
-    position = baseunit.Move(-1, +30);
+	baseunit.Move(-1, +30);
+    position = baseunit.GetPosition();
     ASSERT_TRUE(position.first == -1 && position.second == +30);
 
-    position = baseunit.Move(-13, +30);
+    baseunit.Move(-13, +30);
+    position = baseunit.GetPosition();
     ASSERT_TRUE(position.first == -13 && position.second == +30);
 }
--- Tests/UnitTests/UnitFactoryTests.cpp
+++ Tests/UnitTests/UnitFactoryTests.cpp
@@ -9,20 +9,20 @@ TEST(UnitFactory, CanCreatUnit)
     UnitFactory factory{ 1, 1 };
 
     Setter* setter =
-        dynamic_cast<Setter*>(factory.CreateUnit(Unit::Codes::Setter));
+        dynamic_cast<Setter*>(factory.CreateUnit(UnitCodes::Setter));
     ASSERT_NE(setter, static_cast<Setter*>(nullptr));
     delete (setter);
 
-    setter = dynamic_cast<Setter*>(factory.CreateUnit(Unit::Codes::Worker));
+    setter = dynamic_cast<Setter*>(factory.CreateUnit(UnitCodes::Worker));
     ASSERT_EQ(setter, static_cast<Setter*>(nullptr));
     delete (setter);
 
     Worker* worker =
-        dynamic_cast<Worker*>(factory.CreateUnit(Unit::Codes::Worker));
+        dynamic_cast<Worker*>(factory.CreateUnit(UnitCodes::Worker));
     ASSERT_NE(worker, static_cast<Worker*>(nullptr));
     delete (worker);
 
-    worker = dynamic_cast<Worker*>(factory.CreateUnit(Unit::Codes::Setter));
+    worker = dynamic_cast<Worker*>(factory.CreateUnit(UnitCodes::Setter));
     ASSERT_EQ(worker, static_cast<Worker*>(nullptr));
     delete (worker);
 }
